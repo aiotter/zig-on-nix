5f52ed9d9503d4deaffba104268e183e4bd3d934
aiotter <git@aiotter.com>
2022-07-24 15:13:09 +0900

    feat: Use flake in default.nix

--- default.nix
+++ default.nix
@@ -1,14 +1,8 @@
 self: super:
 
 let
-  nixpkgs = import (builtins.fetchGit {
-    name = "nixpkgs-unstable-2022-07-15";
-    url = "https://github.com/nixos/nixpkgs/";
-    ref = "refs/heads/nixpkgs-unstable";
-    rev = "4bc1c1bd2f5b1101443fe775bb737a812fc8b925";
-  }) {};
-  callPackage = if super.stdenv.isDarwin then nixpkgs.darwin.apple_sdk_11_0.callPackage else super.callPackage;
+  flake = builtins.getFlake (toString ./.);
 in
 {
-  zig = callPackage ./zig.nix { llvmPackages = super.llvmPackages_13; };
+  zig = flake.packages.${builtins.currentSystem}.default;
 }
